58f787ed32b6057816ca036d7cddb301d9f412a9
Soumodeep Roy <83583312+soumodeepRoy853@users.noreply.github.com>
2024-08-25 17:57:04 +0530

    first version

---
+++ keyword.js
@@ -0,0 +1,20 @@
+const key = document.querySelector('#insert')
+
+window.addEventListener('keydown', (e)=>{
+    insert.innerHTML = `
+    <div class = "color">
+    <table>
+  <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    </div>
+    `
+})
